3a2d8b716f44df6429543b2b8d62533f7236ca0f
ericoborgonove <borgonove@gmail.com>
2025-08-02 10:30:11 -0400

    feat(sobre): adição do css

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -1,12 +1,22 @@
 import { Link } from "react-router-dom";
 
+const estiloCaixa={
+    backgroundColor: "#e0e7ff",
+    padding: "20px",
+    borderRadius: "10px",
+    margin: "20px"
+};
+
 const Sobre = () =>{
     return (
         <div style={{textAlign:"center"}}>
             <img src="/vite.svg" alt="icone" />
             <h1>Sobre Nós</h1>
-            <p>Essa página é a página sobre</p>
-            <p>Criamos com todo amor e carinho e um pouco de ódio</p>
+            <div style={estiloCaixa}>
+                <p>Essa página é a página sobre</p>
+                <p>Criamos com todo amor e carinho e um pouco de ódio</p>
+            </div>
+
             <Link to="/">Home</Link>
         </div>
     )
